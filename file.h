d59bee6e1ff9377d7468e1e90cf3b396ac067e6c
minicarrot <97352804+markovochka@users.noreply.github.com>
2024-09-26 12:56:45 +0200

    Update file.h

--- file.h
+++ file.h
@@ -10,25 +10,7 @@ void free_V(struct Vector* x);
 double char_converter(struct Vector* x, int start, int end);
 void char_set(int* c, int* i, Vector* x);
 void straight_line(struct Vector* x);
-{
-    int size;
-    size = x->size-1;
-   for(int i = 0; i <= size; i++)
-   {
-       if(*x->chr[i] == 32)
-       {
-           for(int j = i; j <= size-1; j++)
-           {
-               *x->chr[j] = *x->chr[j+1];
-               *x->chr[j+1] = 32;
-               
-           }
-           if(*x->chr[size] == 32)
-           {
-               *x->chr[size] = 85;
-           }
-       }
-   }
+
 //math operators
 double power(double num, double pwr);
 double n_log(double num);
